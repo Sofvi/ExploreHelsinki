014a014e71f3d3f51d7ece637844280745924564
Sofvi <laitinen.suvi99@gmail.com>
2022-02-15 23:16:46 +0200

    Luotu välilehti

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+/*function nav() {
+  const links = document.getElementById('navbar').getElementsByTagName('a');
+  const current = location.href;
+  for (var i=0; i < links.length; i++) {
+    if (links[i].href === current) {
+      links[i].href = "";
+      links[i].className='graystyle';
+    }
+  }
+}*/
+
+document.getElementById("navbar").onclick = function () {
+  location.href = "html/wlan.html";
+};
