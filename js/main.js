b2956970c6ee705c075e4fee2b271a2b84ca88d6
Sofvi <laitinen.suvi99@gmail.com>
2022-02-18 10:34:22 +0200

    Kotinappi korjattu

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@
 
 //Saa INFO napin toimimaan
 document.getElementById("navbar").onclick = function () {
-  location.href = "html/info.html";
+  location.href = "../info.html";
 };
+
+/*function myFunction() {
+  let i = document.getElementById("navbar");
+  if (i.className === "navbar") {
+    i.className += " responsive";
+  } else {
+    i.className = "navbar";
+  }
+}*/
