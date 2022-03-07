962aff0329a89df1f2b4548449e2f3d8708505ca
nafitus <nafisuln@metropolia.fi>
2022-03-07 17:33:05 +0200

    wlan kartta valmis

--- js/wlan.js
+++ js/wlan.js
@@ -5,10 +5,17 @@ document.getElementById("navbar").onclick = function () {
   location.href = "info.html";
 };
 
+
 const map = L.map('map');
+const wifiIcon = L.icon({
+  iconUrl: 'img/wifi-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+},
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+}).addTo(map));
 
 
 // Asetukset paikkatiedon hakua varten (valinnainen)
@@ -24,7 +31,7 @@ function success(pos) {
   map.setView([crd.latitude, crd.longitude], 12);
 
   L.marker([crd.latitude, crd.longitude]).addTo(map)
-  .bindPopup('Olet tässä', {closeOnClick: false, autoClose: false}).openPopup();
+    .bindPopup('Olet tässä', { closeOnClick: false, autoClose: false }).openPopup();
 }
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
@@ -36,17 +43,47 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 // funktio, jonka avulla voi lisätä pisteitä kartalle
 function lisaaPiste(longitude, latitude, nimi) {
-  return L.marker([latitude, longitude]).
-      addTo(map).bindPopup(nimi).openPopup();
+  return L.marker([latitude, longitude], {icon: wifiIcon}).
+    addTo(map).bindPopup(nimi).openPopup();
+}
+function piste(longitude, latitude) {
+  return L.marker([latitude, longitude], {icon: wifiIcon}).
+    addTo(map);
 }
 
 
 async function otaWlan() {
-  const url = 'https://api.hel.fi/servicemap/v2/search/?q=wlan-sisäpisteet'
-  const vastaus = await fetch(url);
+  const vastaus = await fetch('internet.geojson');
   const data = await vastaus.json();
   console.log(data)
+  for (let i = 0; i < data.features.length; i++) {
+    if (data.features[i].properties.name != null) {
+      const nimi = data.features[i].properties.name;
+      lisaaPiste(data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1], nimi)
+    }
+    else {
+      piste(data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1])
+    }
 
+  }
+
+  async function otaWifi() {
+    const answer = await fetch('wifi.geojson');
+    const wifi = await answer.json();
+    console.log(wifi)
+    for (let i = 0; i < wifi.features.length; i++) {
+      if (wifi.features[i].properties.name != null) {
+        const name = wifi.features[i].properties.name;
+        lisaaPiste(wifi.features[i].geometry.coordinates[0], wifi.features[i].geometry.coordinates[1], name)
+      }
+      else {
+        piste(wifi.features[i].geometry.coordinates[0], wifi.features[i].geometry.coordinates[1])
+      }
+    }
+  }
+  otaWifi();
 }
 
 otaWlan();
+
+
